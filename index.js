79ff601641696c7c20f7bce82ddda1cfe4433e08
Cinta Marquez <mariadelacinta.marquez@gmail.com>
2022-08-05 12:05:03 +0200

    done

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ document.querySelector('#btn').addEventListener('click', () =>{
     let r;
     if(n1 && n2){
         r = n2/ (n1*n1)
-    }else if( !n1 || !n2){
-        r = "error"
+    }else if( !n1 ){
+        r = "Introduce your height"
+    }else if(!n2){
+        r = "Introduce your weight"
     }
-    
     document.querySelector('#result').innerHTML = r;
 });
